4f89122f25d3d11607c43300bea53bd54815279b
Immanuel Currah <immanueli.currah@gmail.com>
2023-04-30 01:03:56 -0400

    socket io working with angular

---
+++ pointer-poker-app/src/app/components/document-list/document-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+import { DocumentService } from 'src/app/services/document.service';
+
+@Component({
+  selector: 'app-document-list',
+  templateUrl: './document-list.component.html',
+  styleUrls: ['./document-list.component.scss'],
+})
+export class DocumentListComponent implements OnInit, OnDestroy {
+  documents: Observable<string[]>;
+  currentDoc: string;
+  private _docSub: Subscription;
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.documents = this.documentService.documents;
+    this._docSub = this.documentService.currentDocument.subscribe((doc) => {
+      if (doc.id) {
+        this.currentDoc = doc.id;
+        console.log(doc.id, doc);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this._docSub.unsubscribe();
+  }
+
+  loadDoc(id: string): void {
+    this.documentService.getDocument(id);
+  }
+
+  newDoc(): void {
+    this.documentService.newDocument();
+  }
+}
